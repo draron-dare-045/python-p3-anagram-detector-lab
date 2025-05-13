122daa52a96905626bd5a9abeb3c78a8c0048356
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-05-13 13:49:29 +0300

    Passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+# lib/anagram.py
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, possible_anagrams):
+        # Sort the characters of the word and check for matches in the list
+        sorted_word = sorted(self.word)
+        return [anagram for anagram in possible_anagrams if sorted(anagram) == sorted_word]
